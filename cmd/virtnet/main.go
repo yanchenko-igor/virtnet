b43e5cfcc76bb2617f4891c895ad31a43b94aceb
Igor Yanchenko <igor.yanchenko@agorism.network>
2026-08-20 14:10:23 -0300

    feat: -topology flag to load JSON topology file
    CLI now accepts -topology <file.json> to load custom topologies
    instead of the hardcoded §15 reference topology. The -start flag
    still overrides the simulation start time.

--- cmd/virtnet/main.go
+++ cmd/virtnet/main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -11,12 +12,15 @@ import (
 	"time"
 
 	"github.com/charmbracelet/bubbletea"
+	"github.com/yanchenko-igor/virtnet/internal/clock"
 	"github.com/yanchenko-igor/virtnet/internal/lab"
+	"github.com/yanchenko-igor/virtnet/internal/topology"
 	"github.com/yanchenko-igor/virtnet/internal/ui"
 )
 
 func main() {
 	start := flag.String("start", "", "simulation start timestamp (RFC3339 like 2026-01-15T08:00:00Z, or Unix seconds); defaults to the Unix epoch")
+	topoFile := flag.String("topology", "", "path to JSON topology file; if empty, uses the §15 reference topology")
 	flag.Parse()
 
 	startTime, err := parseStart(*start)
@@ -25,10 +29,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	l, err := lab.New15At(startTime)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	var l *lab.Lab
+	if *topoFile != "" {
+		data, err := os.ReadFile(*topoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "read topology:", err)
+			os.Exit(1)
+		}
+		var topo topology.Topology
+		if err := json.Unmarshal(data, &topo); err != nil {
+			fmt.Fprintln(os.Stderr, "parse topology:", err)
+			os.Exit(1)
+		}
+		l, _, _, err = topo.BuildLab()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "build lab:", err)
+			os.Exit(1)
+		}
+		// Override clock start time if -start was provided
+		if *start != "" {
+			l.Clock = clock.NewAt(startTime)
+		}
+	} else {
+		l, err = lab.New15At(startTime)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 
 	p := tea.NewProgram(ui.New(l), tea.WithAltScreen())
